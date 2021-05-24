573fb99dd5a82be1e1d31404272b65977514e755
Marcelo Rios <chelo38@gmail.com>
2021-05-24 13:33:10 -0400

    Initial commit

---
+++ client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting client.");
+            System.Threading.Thread.Sleep(2000);
+
+            try
+            {
+                IPEndPoint srvAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+                TcpClient client = new TcpClient();
+                client.Connect(srvAddress);
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                Console.WriteLine("\tClient ready and conected.\n");
+
+                string s = string.Empty;
+                while(!s.Equals("Exit"))
+                {
+                    Console.Write("\tMessage>\t");
+                    s = Console.ReadLine();
+                    writer.WriteLine(s);
+                    writer.Flush();
+
+                    if (!s.Equals("Exit"))
+                    {
+                        string response = reader.ReadLine();
+                        Console.WriteLine("\tResponse:\t" + response);
+                    }
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+                Console.WriteLine("\nConection ended.");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server
+{
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server.");
+            TcpListener listener = null;
+            try
+            {
+                IPEndPoint srvAddress = new IPEndPoint(IPAddress.Loopback, 9000);
+                listener = new TcpListener(srvAddress);
+                listener.Start();
+                Console.WriteLine("\tServer started.");
+
+                while(true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("\tWaiting for connections...\n");
+                    TcpClient client = listener.AcceptTcpClient();
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    StreamWriter writer = new StreamWriter(client.GetStream());
+
+                    string s = string.Empty;
+                    while( !(s = reader.ReadLine()).Equals("Exit") || (s==null))
+                    {
+                        Console.WriteLine("\tMessage:\t" + s);
+                        Console.Write("\tResponse>\t");
+                        String r = Console.ReadLine();
+                        Console.WriteLine("\t--Waiting--");
+                        writer.WriteLine(r + " (Original message: " + s + ")");
+                        writer.Flush();
+                    }
+                    reader.Close();
+                    writer.Close();
+                    client.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if(listener !=null)
+                {
+                    listener.Stop();
+                }
+            }
+
+        }
+    }
+}
